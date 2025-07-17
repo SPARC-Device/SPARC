156d832879b0e668d07339f7f18b33b4bef41909
praful7890 <praful7890@gmail.com>
2025-07-18 03:00:33 +0530

    gui+settings fixed

--- hardware/final_1/setting_2.h
+++ hardware/final_1/setting_2.h
@@ -9,5 +9,6 @@ void setting2Loop();
 
 // If you want to expose any config or UI functions, declare them here
 // For now, only the main entry points are declared
+void drawT9Cell(int index, bool highlightYellow, bool blinkMode = false);
 
 #endif // SETTING_2_H 
